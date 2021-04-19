d88625d1715b75f9327c81c2cdf3c22f857fbc5a
Lukers7 <80413586+Lukers7@users.noreply.github.com>
2021-04-19 12:47:03 +0200

    test commit to see if still works

--- MavenSudokuProjekt1/src/main/java/application/GUI.java
+++ MavenSudokuProjekt1/src/main/java/application/GUI.java
@@ -53,7 +53,7 @@ public class GUI extends Application  {
 		});
 	    
 	}
-	    //this is test circle
+	    //test commit if this still works
 	   
 	    private void closeProgram() {
 	    	CloseWindowStage c = new CloseWindowStage();
